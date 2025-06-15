d0d09856ce3f3e0b434eed72f79d65b87520371d
Bohdan Voitov <bohdan.voiyov@gmail.com>
2025-06-15 07:34:01 +0300

    add_server_models+chat+message

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const chatsRoutes = require("./routes/chats");
+const messagesRoutes = require("./routes/messages");
+
+app.use("/api/chats", chatsRoutes);
+app.use("/api/chats/:chatId/messages", messagesRoutes);
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() =>
+    app.listen(process.env.PORT, () =>
+      console.log(`✅ Server on http://localhost:${process.env.PORT}`)
+    )
+  )
+    .catch((err) => console.error("❌ Mongo error:", err));
+      
+
